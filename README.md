043a5d75208180aeb89f1e3075f04e202b829ece
Ilya Pavlov <iluxz@mail.ru>
2019-01-31 21:46:40 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+App::DTWMIC - Blah blah blah
+
+# SYNOPSIS
+
+    use App::DTWMIC;
+
+# DESCRIPTION
+
+App::DTWMIC is
+
+# AUTHOR
+
+Ilya Pavlov <ilux@cpan.org>
+
+# COPYRIGHT
+
+Copyright 2019- Ilya Pavlov
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/App/DTWMIC.pm
@@ -0,0 +1,39 @@
+package App::DTWMIC;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+App::DTWMIC - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use App::DTWMIC;
+
+=head1 DESCRIPTION
+
+App::DTWMIC is
+
+=head1 AUTHOR
+
+Ilya Pavlov E<lt>ilux@cpan.orgE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2019- Ilya Pavlov
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
